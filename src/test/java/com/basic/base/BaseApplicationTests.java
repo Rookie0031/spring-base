b195064cda03a504a25c40233c22a90f994de9ad
Declan <103009135+Rookie0031@users.noreply.github.com>
2025-04-27 15:54:43 +0900

    add postgresql db settings

--- src/test/java/com/basic/base/BaseApplicationTests.java
+++ src/test/java/com/basic/base/BaseApplicationTests.java
@@ -1,13 +1,12 @@
 package com.basic.base;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
